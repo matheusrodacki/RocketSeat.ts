7ef1c220b8d67d48c706124fdcebbe78ef674b68
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-09-04 04:46:45 +0000

    type hellow word

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('hello, world!');
+type Usuario = {
+
+    anoQueNasceu: number
+}
+
+function calculaIdadeDoUsuario(usuario: Usuario){
+
+    return 2023 - usuario.anoQueNasceu;
+}
+
+console.log(calculaIdadeDoUsuario({anoQueNasceu: 1994}));
+
